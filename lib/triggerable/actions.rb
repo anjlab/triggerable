a2f5ca93ae242216c5e674fca3f9ac1ea690c0a7
DmitryTsepelev <dmitry.a.tsepelev@gmail.com>
2014-12-11 17:26:41 +0300

    v0.1.7 fix paper_trail integration

--- lib/triggerable/actions.rb
+++ lib/triggerable/actions.rb
@@ -22,9 +22,19 @@ module Triggerable
     def run_for! object, trigger_name
       proc = @block
       object.instance_eval do
-        PaperTrail.whodunnit = trigger_name if defined?(PaperTrail)
-        instance_exec(&proc)
-        PaperTrail.whodunnit = nil if defined?(PaperTrail)
+        change_whodunnit = trigger_name.present? && defined?(PaperTrail)
+        old_whodunnit = nil
+
+        if change_whodunnit
+          old_whodunnit = PaperTrail.whodunnit
+          PaperTrail.whodunnit = trigger_name
+        end
+
+        begin
+          instance_exec(&proc)
+        ensure
+          PaperTrail.whodunnit = old_whodunnit if change_whodunnit
+        end
       end
     end
   end
